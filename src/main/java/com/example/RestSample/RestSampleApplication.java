4654ded38b7cab7d87aece37d99a42b394725f9a
NIRI-1996 <gnr.gtr96@gmail.com>
2021-08-20 01:03:01 +0530

    rest

--- src/main/java/com/example/RestSample/RestSampleApplication.java
+++ src/main/java/com/example/RestSample/RestSampleApplication.java
@@ -2,10 +2,10 @@ package com.example.RestSample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages= {"com.example.RestSample"})
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class RestSampleApplication {
 
 	public static void main(String[] args) {
