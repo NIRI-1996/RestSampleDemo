1c12a006ded14937a31c1aa621f5f00229667b0e
NIRI-1996 <gnr.gtr96@gmail.com>
2021-08-24 15:09:51 +0530

    "rest modified"

--- src/main/java/com/example/RestSample/Controller/StudentsDetailsController.java
+++ src/main/java/com/example/RestSample/Controller/StudentsDetailsController.java
@@ -1,6 +1,7 @@
 package com.example.RestSample.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,20 +21,24 @@ public class StudentsDetailsController {
 	private StudentDetailsRepo detailsRepo;
 	
 	@PostMapping("/details")
-	public StudentDetails details(@RequestBody StudentDetails studentDetails) {
+	public StudentDetails savedetails(@RequestBody StudentDetails studentDetails) {
 		return detailsRepo.save(studentDetails);
 		
 	}
 	
-	@PutMapping("/updatedetails/{id}")
-	public StudentDetails updatedetails(@PathVariable int id) {
-		StudentDetails sd=detailsRepo.findById(id).get();
-    return detailsRepo.save(sd);
-		
-	}
 	@GetMapping("/getdetails/{id}")
 	public StudentDetails getdetails(@PathVariable int id) {
 		return detailsRepo.findById(id).get();
-		
+	
 	}
+	@PutMapping("/updatedetails/{id}")
+	public void updatedetails(@RequestBody StudentDetails details) {
+		detailsRepo.save(details);
+	}
+	
+	@DeleteMapping("/deletedetails/{id}")
+	public void deletestudent(@PathVariable int id) {
+		detailsRepo.deleteById(id);
+	}
+
 }
